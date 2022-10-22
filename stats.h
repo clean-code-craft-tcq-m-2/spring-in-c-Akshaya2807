6e6c6774ddae1a5d0c50f647a90fe9efa1c7c4e4
Akshaya2807 <115683252+Akshaya2807@users.noreply.github.com>
2022-10-22 20:47:54 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -12,5 +12,4 @@ void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats_t com
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
-extern int emailalerter;
-extern int ledAlerter;
+
